c1eaefe181e3382ac2b7d950f0be19d0b88c8ad0
fady emil <fady46t45f@gmail.com>
2024-10-12 12:56:35 +0300

    create  pagination for local data source

--- lib/features/characters/data/data_sourse/charaters_local_data_source.dart
+++ lib/features/characters/data/data_sourse/charaters_local_data_source.dart
@@ -3,13 +3,27 @@ import 'package:the_rich_and_morty/core/constant/box_name.dart';
 import 'package:the_rich_and_morty/features/characters/domain/entities/charaters_entities.dart';
 
 abstract class CharatersLocalDataSource {
-  List<CharatersEntities> fetchCharaters();
+  List<CharatersEntities> fetchCharaters({int pageNumber = 0});
 }
 
 class CharatersLocalDataSourceImpl extends CharatersLocalDataSource {
   @override
-  List<CharatersEntities> fetchCharaters() {
+  List<CharatersEntities> fetchCharaters({int pageNumber = 0}) {
+    int startIndex = pageNumber * 20;
+    int endIndex = (pageNumber + 1) * 20;
+
     var box = Hive.box<CharatersEntities>(BoxName.kCharactersBox);
-    return box.values.toList();
+    int length = box.values.length;
+
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+
+    return box.values.toList().sublist(startIndex, endIndex);
   }
 }
+//  @override
+//   List<CharatersEntities> fetchCharaters() {
+//     var box = Hive.box<CharatersEntities>(BoxName.kCharactersBox);
+//     return box.values.toList();
+//   }
